2406e431fd6651be9ec7b3b9e5a9f4167c0ec89d
yumii-no-do <yumii.do.no@gmail.com>
2018-02-01 18:08:11 +0300

    слил с work

---
+++ js/ships.js
@@ -0,0 +1,374 @@
+var data = [
+    {
+        "id": 1,
+        "companyID": 1,
+        "company": "rsi",
+        "class": 1,
+        "mass": 100,
+        "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "Honer",
+        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+        "pop": 10
+    },
+    {
+        "id": 2,
+        "companyID": 1,
+        "company": "argis",
+        "class": 2,
+        "mass": 200,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "people": 1, "pop": 9,
+        "link": "../img/p2-2.png",
+        "name": "lololo"
+    },
+    {
+        "id": 3,
+        "companyID": 3,
+        "company": "anvil",
+        "class": 2,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 200, "pop": 8,
+        "people": 1,
+        "link": "../img/p2-3-2.png",
+        "name": "Hofew3"
+    },
+    {
+        "id": 4,
+        "companyID": 4,
+        "company": "misc",        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "class": 3, "pop": 7,
+        "mass": 300, "people": 2,
+        "link": "../img/p2-4-1.png",
+        "name": "waef"
+    },
+    {
+        "id": 5,
+        "companyID": 5,
+        "company": "origin",
+        "class": 4, "pop": 5,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 400, "people": 2,
+        "link": "../img/p2-4-2.png",
+        "name": "Hereerg"
+    },
+    {
+        "id": 6,
+        "companyID": 6,
+        "company": "drake",        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "class": 3, "pop": 4,
+        "mass": 300, "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "ewfwe"
+    },
+    {
+        "id": 7,
+        "companyID": 4,
+        "company": "misc",
+        "class": 2, "pop": 4,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 200, "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "wefwew"
+    },
+    {
+        "id": 8,
+        "companyID": 5,
+        "company": "origin",        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "class": 1, "pop": 3,
+        "mass": 100, "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "awef"
+    },
+    {
+        "id": 9,
+        "companyID": 6,
+        "company": "drake",        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "class": 2, "pop": 2,
+        "mass": 200, "people": 2,
+        "link": "../img/p2-2.png",
+        "name": "Hwegrfv"
+    },
+    {
+        "id": 10,
+        "companyID": 6,
+        "company": "anvil",        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "class": 2, "pop": 1,
+        "mass": 200, "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "Hwed"
+    },
+    {
+        "id": 11,
+        "companyID": 1,
+        "company": "rsi",        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "class": 1,
+        "mass": 100,
+        "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "Honer",
+        "pop": 10
+    },
+    {
+        "id": 12,
+        "companyID": 1,
+        "company": "argis",        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "class": 2,
+        "mass": 200,
+        "people": 1, "pop": 9,
+        "link": "../img/p2-2.png",
+        "name": "lololo"
+    },
+    {
+        "id": 13,
+        "companyID": 3,
+        "company": "anvil",
+        "class": 2,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 200, "pop": 8,
+        "people": 1,
+        "link": "../img/p2-3-2.png",
+        "name": "Hofew3"
+    },
+    {
+        "id": 14,
+        "companyID": 4,
+        "company": "misc",
+        "class": 3, "pop": 7,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 300, "people": 2,
+        "link": "../img/p2-4-1.png",
+        "name": "waef"
+    },
+    {
+        "id": 15,
+        "companyID": 5,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "company": "origin",
+        "class": 4, "pop": 5,
+        "mass": 400, "people": 2,
+        "link": "../img/p2-4-2.png",
+        "name": "Hereerg"
+    },
+    {
+        "id": 16,
+        "companyID": 6,
+        "company": "drake",
+        "class": 3, "pop": 4,
+        "mass": 300, "people": 1,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "link": "../img/p2-2.png",
+        "name": "ewfwe"
+    },
+    {
+        "id": 17,
+        "companyID": 4,
+        "company": "misc",
+        "class": 2, "pop": 4,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 200, "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "wefwew"
+    },
+    {
+        "id": 18,
+        "companyID": 5,
+        "company": "origin",
+        "class": 1, "pop": 3,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 100, "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "awef"
+    },
+    {
+        "id": 19,
+        "companyID": 6,
+        "company": "drake",
+        "class": 2, "pop": 2,
+        "mass": 200, "people": 2,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "link": "../img/p2-2.png",
+        "name": "Hwegrfv"
+    },
+    {
+        "id": 20,
+        "companyID": 6,
+        "company": "anvil",
+        "class": 2, "pop": 1,        "text":"To the enemy, the Hornet F7C is a weapon never to be underestimated. To allies, it's a savior. The F7C Hornet is the same dependable and resilient multi-purpose fighter that has become the face of the UEE Navy. The F7C is the foundation to build on and meet whatever requirements you have in mind.",
+
+        "mass": 200, "people": 1,
+        "link": "../img/p2-2.png",
+        "name": "Hwed"
+    }
+
+];
+window.onscroll = function () {
+    logo();
+}
+searchShip();
+
+function searchShip(par) {
+
+        // var content = document.getElementsByClassName("content")[0];
+        //  content.innerHTML = "";
+
+
+    for (var i1 = 0; i1 < data.length; i1++) {
+        add(data[i1].link, data[i1].name, data[i1].id);
+    }
+
+
+        //
+        // var property = propertyFilter();
+        //
+        // var res = data.filter(function (el) {
+        //         return (el.company === property.company);
+        //
+        //
+        // });
+        //
+        // console.log(res);
+        // for (var i = 0; i < res.length; i++) {
+        //     var el = res[i].id;
+        //     add(el.link, el.name, el.id);
+        // }
+
+}
+
+// function propertyFilter() {
+//     var sort = document.querySelector("#selectSort").value;
+//     var company = document.getElementById("selectCompany").value;
+//     // var clas = document.getElementById("selectClass").value;
+//     // var mass = document.getElementById("selectMass").value;
+//     // var people = document.getElementById("selectPeople").value;
+//
+//     return {
+//         "sort": sort,
+//         "company": company
+//         // "clas":clas,
+//         // "mass":mass,
+//         // "people":people
+//     }
+// }
+
+function add(linkImg, text, id) {
+    var content = document.getElementsByClassName("content")[0];
+
+    var shipDiv = document.createElement("div");
+    var imgDiv = document.createElement("img");
+    var imgDiv2 = document.createElement("div");
+    var textDiv = document.createElement("div");
+
+    shipDiv.className = "ship";
+    imgDiv.className = "img";
+    textDiv.className = "text";
+
+    imgDiv.setAttribute("src", linkImg);
+
+    shipDiv.setAttribute("data-id", id);
+    shipDiv.setAttribute("onclick", "moreInf(this)");
+
+    shipDiv.appendChild(imgDiv2);
+    shipDiv.appendChild(textDiv);
+    imgDiv2.appendChild(imgDiv);
+    // console.log(shipDiv);
+
+    textDiv.innerText = text;
+
+    content.appendChild(shipDiv);
+}
+
+// function del() {
+//     var content = document.getElementsByClassName("content")[0];
+//
+//     for (var i = 0; i < content.children.length; i++) {
+//
+//         content.removeChild(content.children[i]);
+//     }
+// }
+
+function scrollTop() {
+    //возвращяет текущий скролл
+    var html = document.documentElement;
+    var body = document.body;
+    var scrollTop = html.scrollTop || body && body.scrollTop || 0;
+    scrollTop -= html.clientTop; // в IE7- <html> смещён относительно (0,0)
+    return scrollTop;
+}
+
+function moreInf(path){
+
+    var content = document.getElementsByClassName("moreInf")[0];
+    var bac = document.getElementsByClassName("bac")[0];
+    var shipDiv = document.createElement("div");
+    var imgDiv = document.createElement("img");
+    var imgDiv2 = document.createElement("div");
+    var titleDiv = document.createElement("div");
+    var textDiv = document.createElement("div");
+    var close = document.createElement("div");
+
+    var el = data[path.dataset.id-1];
+
+    // content.id = "show";
+    bac.id="show";
+
+    shipDiv.className = "moreInfShip";
+    shipDiv.id = "show";
+    imgDiv.className = "moreInfImg";
+    textDiv.className = "moreInfText";
+    titleDiv.className = "moreInfTitle";
+    close.className = "moreInfClose";
+
+    imgDiv.setAttribute("src", el.link);
+    close.setAttribute("onclick", "moreInfClose()");
+
+    shipDiv.appendChild(imgDiv2);
+    shipDiv.appendChild(titleDiv);
+    shipDiv.appendChild(textDiv);
+    shipDiv.appendChild(close);
+    imgDiv2.appendChild(imgDiv);
+    console.log(shipDiv);
+
+    titleDiv.innerText = el.name;
+    textDiv.innerText = el.text;
+    close.innerText = "+";
+
+    content.appendChild(shipDiv);
+}
+
+function moreInfClose(){
+
+    var content = document.getElementsByClassName("moreInf")[0];
+    var bac = document.getElementsByClassName("bac")[0];
+
+    content.id = "hide";
+    bac.id="hide";
+
+    content.innerHTML = "";
+}
+
+function find(id) {
+    for (var i = 0; i < data.length; i++) {
+
+        return (data[i].id === id )? i: undefined;
+    }
+}
+
+function logo() {
+    var scroll = scrollTop();
+    var logo = document.getElementById("logo").children[0].children[0];
+
+    logo.style.filter = "invert(1)";
+    if (scroll > 120) {
+        logo.style.filter = "invert(0)";
+    }
+
+
+}
